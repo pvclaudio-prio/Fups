fa821f1a9e281180743df45066c8dee397053b0c
pvclaudio-prio <cvieira@prio3.com.br>
2025-05-13 16:16:21 -0300

    Update fup.py

--- fup.py
+++ fup.py
@@ -252,7 +252,6 @@ if menu == "Dashboard":
         st.subheader("📅 Follow-ups por Ano")
         ano_counts = df["Ano"].value_counts().sort_index().reset_index()
         ano_counts.columns = ["Ano", "Quantidade"]
-        ano_counts["Ano"] = int(ano_counts["Ano"])
         fig_ano = px.line(
             ano_counts,
             x="Ano",
