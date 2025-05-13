80ce78f0a839c055af7158efe62900b801a6d3f1
pvclaudio-prio <cvieira@prio3.com.br>
2025-05-13 14:24:38 -0300

    Update fup.py

--- fup.py
+++ fup.py
@@ -284,7 +284,7 @@ elif menu == "Meus Follow-ups":
         caminho_temp = tempfile.NamedTemporaryFile(delete=False).name
         arquivo.GetContentFile(caminho_temp)
 
-        df = pd.read_csv(caminho_temp)
+        pd.read_csv(caminho_temp, sep=";", encoding="utf-8")
         st.write("🧾 Colunas disponíveis:", df.columns.tolist())
 
         usuario_logado = st.session_state.username
