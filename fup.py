3e941efae176de05b522830a650b2819d805f7c2
pvclaudio-prio <cvieira@prio3.com.br>
2025-05-14 07:45:38 -0300

    Update fup.py

--- fup.py
+++ fup.py
@@ -163,7 +163,8 @@ users = {
     "amendonca": {"name": "Alex Mendonça", "password": "1234"},
     "mathayde": {"name": "Maria Luiza", "password": "1234"},
     "bella": {"name": "Isabella Miranda", "password": "1234"},
-    "ysouza": {"name": "Yasmim Souza", "password": "compliance"}
+    "ysouza": {"name": "Yasmim Souza", "password": "compliance@2025"},
+    "alrodrigues": {"name": "Aline Rodrigues", "password": "sms@2025"}
 }
 
 if "logged_in" not in st.session_state:
