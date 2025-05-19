9266f54204b1009598e96cf6734722aea05e0d51
pvclaudio-prio <cvieira@prio3.com.br>
2025-05-19 10:29:34 -0300

    Update fup.py

--- fup.py
+++ fup.py
@@ -316,7 +316,7 @@ elif menu == "Meus Follow-ups":
 
         if usuario_logado not in admin_users:
             df = df[df["Responsavel"].str.lower() == nome_usuario.lower()]
-        df["Prazo"] = pd.to_datetime(df["Prazo"])
+        df["Prazo"] = pd.to_datetime(df["Prazo"], format = "mixed")
         df["Ano"] = df["Ano"].astype(str)
 
         # --- Filtros na sidebar ---
