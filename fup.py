e03985cd5666763de0c48ed86ce65b6d6d90fb9d
pvclaudio-prio <cvieira@prio3.com.br>
2025-05-20 10:43:41 -0300

    Update fup.py

--- fup.py
+++ fup.py
@@ -282,8 +282,6 @@ if menu == "Dashboard":
         st.subheader("📁 Follow-ups por Auditoria")
         auditoria_counts = df["Auditoria"].value_counts().reset_index()
         auditoria_counts.columns = ["Auditoria", "Quantidade"]
-        anos_existentes = ano_counts["Ano"].tolist()
-        ano_counts["Ano"] = pd.Categorical(ano_counts["Ano"], categories=anos_existentes, ordered=True)
 
         fig_auditoria = px.bar(
             auditoria_counts,
@@ -296,6 +294,8 @@ if menu == "Dashboard":
         st.subheader("📅 Follow-ups por Ano")
         ano_counts = df["Ano"].value_counts().sort_index().reset_index()
         ano_counts.columns = ["Ano", "Quantidade"]
+        anos_existentes = ano_counts["Ano"].tolist()
+        ano_counts["Ano"] = pd.Categorical(ano_counts["Ano"], categories=anos_existentes, ordered=True)
         
         fig_ano = px.line(
             ano_counts,
